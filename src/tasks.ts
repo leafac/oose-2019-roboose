78523a04ca5d83fe835f2c4ffb688fadbef6f565
Leandro Facchinetti <git@leafac.com>
2019-08-14 19:29:17 -0400

    Start tasks

--- src/tasks.ts
+++ src/tasks.ts
@@ -4,7 +4,9 @@ const program = new Command();
 
 program
   .command("initialize")
-  .action(() => {
-  });
+  .description("create the repositories for staff and students")
+  .action(() => {});
+
+if (process.argv.length === 2) process.argv.push("--help");
 
 program.parse(process.argv);
