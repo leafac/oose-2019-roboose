10dec5429be932acea0cd1ef5b8f5eb9c1407787
Leandro Facchinetti <git@leafac.com>
2019-08-14 19:35:24 -0400

    Add dotenv

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,12 +1,15 @@
 import { Command } from "commander";
+import { config } from "dotenv";
 
 const program = new Command();
 
 program
   .command("initialize")
   .description("create the repositories for staff and students")
-  .action(() => {});
+  .action(() => {
+    console.log(process.env.YEAR);
+  });
 
+config();
 if (process.argv.length === 2) process.argv.push("--help");
-
 program.parse(process.argv);
