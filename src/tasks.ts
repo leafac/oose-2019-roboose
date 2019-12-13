d09d4f41ecf85cfa901ae5289d6ee4eff6906560
Leandro Facchinetti <git@leafac.com>
2019-12-13 13:18:49 -0500

    Work in progress on publishing system

--- src/tasks.ts
+++ src/tasks.ts
@@ -422,7 +422,7 @@ program
   });
 
 program.command("quiz:grades:start").action(async () => {
-  const submissions = [];
+  const submissions = new Array<{ github: string; commit: string }>();
   for (const github of await getStudents()) {
     submissions.push({
       github,
@@ -819,6 +819,17 @@ async function getGroups(): Promise<string[]> {
     .map(team => team.slug);
 }
 
+async function getStaff(): Promise<string[]> {
+  return (await octokit.paginate(
+    octokit.teams.listMembers.endpoint.merge({
+      team_id: (await octokit.teams.getByName({
+        org: "jhu-oose",
+        team_slug: `${process.env.COURSE}-staff`
+      })).data.id
+    })
+  )).map(member => member.login);
+}
+
 async function getStaffFile(path: string): Promise<string> {
   return Buffer.from(
     (await octokit.repos.getContents({
@@ -988,6 +999,145 @@ ${submissions
 `
     });
   }
+}
+
+async function publishStudentsGrades(
+  subject: string,
+  gradesPath: string
+): Promise<void> {
+  const title = `Grade for ${subject}`;
+  for (const [github, grade] of await computeGrades(gradesPath)) {
+    try {
+      if (
+        (await octokit.paginate(
+          octokit.issues.listForRepo.endpoint.merge({
+            owner: "jhu-oose",
+            repo: `${process.env.COURSE}-student-${github}`
+          })
+        )).some(issue => issue.title === title)
+      )
+        continue;
+      await octokit.issues.create({
+        owner: "jhu-oose",
+        repo: `${process.env.COURSE}-student-${github}`,
+        title,
+        body: grade
+      });
+    } catch (error) {
+      console.log(`Error with ${github}: ${error}`);
+    }
+  }
+  await octokit.issues.updateMilestone({
+    owner: "jhu-oose",
+    repo: `${process.env.COURSE}-staff`,
+    milestone_number: (await octokit.paginate(
+      octokit.issues.listMilestonesForRepo.endpoint.merge({
+        owner: "jhu-oose",
+        repo: `${process.env.COURSE}-staff`
+      })
+    )).find(milestone => milestone.title === `Grade ${subject}`).number,
+    state: "closed"
+  });
+}
+
+type GitHub = string;
+type Grade = string;
+
+async function computeGrades(gradesPath: string): Promise<Map<GitHub, Grade>> {
+  const staff = await getStaff();
+  const partsGradesMappings = new Array<Map<GitHub, Grade>>();
+  for (const partPath of await listStaffDirectory(gradesPath)) {
+    const [, rubricSection, gradesSection] = (await getStaffFile(
+      `${gradesPath}/${partPath}`
+    )).match(/^# Rubric(.*)^# Grades(.*)/ms)!;
+    type RubricItemName = string;
+    type RubricItemContent = string;
+    const rubric = rubricSection
+      .split(/^##\s*/m)
+      .slice(1)
+      .reduce((rubric, item) => {
+        const [, name, content] = item.match(/(.*?)\n(.*)/s)!;
+        content.split("\n").forEach(line => {
+          if (!line.match(/^\*\*(-|\+)\d+\*\*/) && line !== "")
+            throw `Error in rubric section (missing points?) (Part: ‘${part}’ · Name: ‘${name}’ · Line: ‘${line}’)`;
+        });
+        return rubric.set(name, content);
+      }, new Map<RubricItemName, RubricItemContent>());
+    const partGradesMappings = gradesSection
+      .split(/^##\s*/m)
+      .slice(1)
+      .reduce((partGradesMappings, entry) => {
+        const [, github, url, rawContent] = entry.match(
+          /\[(.*?)\]\((.*?)\)(.*)/s
+        )!;
+        const content = rawContent
+          .split("\n")
+          .map(line => {
+            if (line.match(/^\*\*(-|\+)\d+\*\*/)) {
+              return line;
+            } else if (line.match(/^\*\*Grader:\*\*/)) {
+              const [, grader] = line.match(/^\*\*Grader:\*\*\s*(.*)/)!;
+              if (!staff.includes(grader))
+                throw `Grader ‘${grader}’ isn’t a member of GitHub team ‘${process.env.COURSE}-staff’ (Part: ‘${part}’ · Student: ‘${github}’)`;
+              return line;
+            } else if (line === "") {
+              return line;
+            } else if (rubric.has(line)) {
+              return rubric.get(line);
+            } else {
+              throw `Error in grade section (misuse of rubric?) (Part: ‘${part}’ · Student: ‘${github}’ · Line: ‘${line}’)`;
+            }
+          })
+          .join("\n");
+        return partGradesMappings.set(
+          github,
+          `# [${part}](${url})
+
+${content}
+`
+        );
+      }, new Map<GitHub, Grade>());
+    partsGradesMappings.push(partGradesMappings);
+  }
+  const aggregatedGradesMappings = partsGradesMappings.reduce(
+    (gradesMappings, partGradesMappings) => {
+      const augmentedGradesMappings = new Map();
+      if (gradesMappings.size !== partGradesMappings.size)
+        throw "Different number of students in the grading files for the different parts of the assignment";
+      for (const github of gradesMappings.keys()) {
+        if (!partGradesMappings.has(github))
+          throw `Student ${github} is in one of the grading files, but not the other.`;
+        augmentedGradesMappings.set(
+          github,
+          gradesMappings.get(github)! + partGradesMappings.get(github)!
+        );
+      }
+      return augmentedGradesMappings;
+    }
+  );
+  const totalsGradesMappings = new Map();
+  for (const [github, grade] of aggregatedGradesMappings) {
+    const points = (grade.match(/^\*\*(-|\+)\d+\*\*/gm) || []).map(point =>
+      Number(point.slice("**".length, point.length - "**".length))
+    );
+    const total = points.reduce((a, b) => a + b, 100);
+    totalsGradesMappings.set(
+      github,
+      `${grade}
+
+---
+
+**Total:** ${total}/100
+
+To accept the grade, close this issue.
+
+To request a regrade, comment on this issue within one week. Mention the grader of the part, for example, if the grader of the part is \`jhu-oose-example-ca\`, mention with \`@jhu-oose-example-ca\`.
+
+/cc @${github}
+`
+    );
+  }
+  return totalsGradesMappings;
 }
 
 function render(template: string, scope: object = {}): string {
